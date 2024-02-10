b6eee4b17f03e217cca34d7c0f6ba68216903bee
maybekd <karandeepsingh8860@gmail.com>
2024-02-10 10:50:21 -0500

    Update FinancePal bot with new features and bug fixes

--- cogs/IncomeStatement.py
+++ cogs/IncomeStatement.py
@@ -30,9 +30,10 @@ class PaginationView(nextcord.ui.View):
 
 # Dropdown Select Class
 class Dropdown(nextcord.ui.Select):
-    def __init__(self, annual_reports, interaction: Interaction, cog, logo_url):
+    def __init__(self, symbol, annual_reports, interaction: Interaction, cog, logo_url):
         super().__init__(placeholder="Choose a Fiscal Date", min_values=1, max_values=1, 
                          options=[nextcord.SelectOption(label=report['fiscalDateEnding']) for report in annual_reports])
+        self.symbol = symbol
         self.annual_reports = annual_reports
         self.interaction = interaction
         self.cog = cog
@@ -42,15 +43,15 @@ class Dropdown(nextcord.ui.Select):
         selected_date = self.values[0]
         selected_report = next((report for report in self.annual_reports if report['fiscalDateEnding'] == selected_date), None)
         if selected_report:
-            paginated_embeds = self.cog.create_paginated_embeds_for_report(selected_report, self.logo_url)
+            paginated_embeds = self.cog.create_paginated_embeds_for_report(self.symbol, selected_report, self.logo_url)
             view = PaginationView(paginated_embeds)
             await interaction.response.edit_message(content="Here is the income statement:", embed=paginated_embeds[0], view=view)
 
 # Dropdown View Class
 class DropdownView(nextcord.ui.View):
-    def __init__(self, annual_reports, interaction: Interaction, cog, logo_url):
+    def __init__(self, symbol, annual_reports, interaction: Interaction, cog, logo_url):
         super().__init__()
-        self.add_item(Dropdown(annual_reports, interaction, cog, logo_url))
+        self.add_item(Dropdown(symbol, annual_reports, interaction, cog, logo_url))
 
 # Main Cog Class
 class CompanyInfoTest(commands.Cog):
@@ -89,70 +90,54 @@ class CompanyInfoTest(commands.Cog):
                 return full_name.split(' ')[0]  # Assuming the company name is the first part
         return None
 
-    '''
-    def create_paginated_embeds_for_report(self, report, logo_url):
-        embeds = []
-        items = list(report.items())
-        items_per_page = 13
-
-        for i in range(0, len(items), items_per_page):
-            embed = nextcord.Embed(title=f"Income Statement for {report['fiscalDateEnding']}",
-                                   description=f"Data from {datetime.now().strftime('%Y-%m-%d')}")
-            for key, value in items[i:i+items_per_page]:
-                embed.add_field(name=key, value=value, inline=False)
-            if logo_url:
-                embed.set_thumbnail(url=logo_url)
-            embed.set_footer(text="Data provided by FinancePal Bot")
-            embeds.append(embed)
-        return embeds
-    '''
     
-    def create_paginated_embeds_for_report(self, the_income_info, logo_url):
+    
+    def create_paginated_embeds_for_report(self, symbol, the_income_info, logo_url):
         embeds = []
-
+        bloomberg_url = f"https://www.bloomberg.com/quote/{symbol}:US"
         # Page 1
-        embed1 = nextcord.Embed(title=f"Income Statement for {the_income_info['fiscalDateEnding']}",
-                                description=f"Data from {datetime.now().strftime('%Y-%m-%d')}")
-        # First page fields, corrected for case sensitivity and exact spelling
-        embed1.add_field(name="Reported Currency", value=the_income_info.get('reportedCurrency', 'N/A'), inline=True)
-        embed1.add_field(name="Gross Profit", value=f"${the_income_info.get('grossProfit', 'N/A')}", inline=True)
-        embed1.add_field(name="Total Revenue", value=the_income_info.get('totalRevenue', 'N/A'), inline=False)
-        embed1.add_field(name="Cost Of Revenue", value=the_income_info.get('costOfRevenue', 'N/A'), inline=False)
-        embed1.add_field(name="Cost of Goods and Services Sold", value=the_income_info.get('costofGoodsAndServicesSold', 'N/A'), inline=False)
-        embed1.add_field(name="Operating Income", value=the_income_info.get('operatingIncome', 'N/A'), inline=True)
-        embed1.add_field(name="Selling General and Administrative", value=the_income_info.get('sellingGeneralAndAdministrative', 'N/A'), inline=True)
-        embed1.add_field(name="Research and Development", value=the_income_info.get('researchAndDevelopment', 'N/A'), inline=False)
-        embed1.add_field(name="Operating Expenses", value=the_income_info.get('operatingExpenses', 'N/A'), inline=True)
-        embed1.add_field(name="Investment Income Net", value=the_income_info.get('investmentIncomeNet', 'N/A'), inline=True)
-        embed1.add_field(name="Net Interest Income", value=the_income_info.get('netInterestIncome', 'N/A'), inline=True)
-        embed1.add_field(name="Interest Income", value=the_income_info.get('interestIncome', 'N/A'), inline=True)
+        embed1 = nextcord.Embed(title=f"{symbol} Income Statement", url=bloomberg_url, description=f"Fiscal Report Date: {the_income_info['fiscalDateEnding']}", color=0x4dff4d)
+        embed1.add_field(name="Reported Currency", value=the_income_info.get('reportedCurrency', 'N/A'), inline=False)
+        embed1.add_field(name="Gross Profit", value=f"${the_income_info.get('grossProfit', 'N/A')}", inline=False)
+        embed1.add_field(name="Total Revenue", value=f"${the_income_info.get('totalRevenue', 'N/A')}", inline=False)
+        embed1.add_field(name="Cost Of Revenue", value=f"${the_income_info.get('costOfRevenue', 'N/A')}", inline=False)
+        embed1.add_field(name="Cost of Goods and Services Sold", value=f"${the_income_info.get('costofGoodsAndServicesSold', 'N/A')}", inline=False)
+        embed1.add_field(name="Operating Income", value=f"${the_income_info.get('operatingIncome', 'N/A')}", inline=False)
+        embed1.add_field(name="Selling General and Administrative", value=f"${the_income_info.get('sellingGeneralAndAdministrative', 'N/A')}", inline=False)
+        embed1.add_field(name="Research and Development", value=f"${the_income_info.get('researchAndDevelopment', 'N/A')}", inline=False)
+        embed1.add_field(name="Operating Expenses", value=f"${the_income_info.get('operatingExpenses', 'N/A')}", inline=False)
+        embed1.add_field(name="Investment Income Net", value=f"${the_income_info.get('investmentIncomeNet', 'N/A')}", inline=False)
+        embed1.add_field(name="Net Interest Income", value=f"${the_income_info.get('netInterestIncome', 'N/A')}", inline=False)
+        embed1.add_field(name="Interest Income", value=f"${the_income_info.get('interestIncome', 'N/A')}" , inline=False)
 
         if logo_url:
             embed1.set_thumbnail(url=logo_url)
-        embed1.set_footer(text="Data provided by FinancePal Bot")
+        
+        current_time = datetime.now().strftime('%m/%d/%Y %H:%M %p')
+        embed1.set_footer(text=f"Data provided by FinancePal Bot | {current_time}")
         embeds.append(embed1)
 
         # Page 2
-        embed2 = nextcord.Embed(title=f"Income Statement for {the_income_info['fiscalDateEnding']}",
-                                description=f"Continued...")
-        # Second page fields, corrected for case sensitivity and exact spelling
-        embed2.add_field(name="Interest Expense", value=the_income_info.get('interestExpense', 'N/A'), inline=True)
-        embed2.add_field(name="Non Interest Income", value=the_income_info.get('nonInterestIncome', 'N/A'), inline=True)
-        embed2.add_field(name="Other Non Operating Income", value=the_income_info.get('otherNonOperatingIncome', 'N/A'), inline=True)
-        embed2.add_field(name="Depreciation", value=the_income_info.get('depreciation', 'N/A'), inline=True)
-        embed2.add_field(name="Depreciation and Amortization", value=the_income_info.get('depreciationAndAmortization', 'N/A'), inline=True)
-        embed2.add_field(name="Income Before Tax", value=the_income_info.get('incomeBeforeTax', 'N/A'), inline=True)
-        embed2.add_field(name="Income Tax Expense", value=the_income_info.get('incomeTaxExpense', 'N/A'), inline=True)
-        embed2.add_field(name="Interest and Debt Expense", value=the_income_info.get('interestAndDebtExpense', 'N/A'), inline=True)
-        embed2.add_field(name="Net Income from Continuing Operations", value=the_income_info.get('netIncomeFromContinuingOperations', 'N/A'), inline=True)
-        embed2.add_field(name="Comprehensive Income Net of Tax", value=the_income_info.get('comprehensiveIncomeNetOfTax', 'N/A'), inline=True)
-        embed2.add_field(name="ebit", value=the_income_info.get('ebit', 'N/A'), inline=True)
-        embed2.add_field(name="ebitda", value=the_income_info.get('ebitda', 'N/A'), inline=True)
-        embed2.add_field(name="Net Income", value=the_income_info.get('netIncome', 'N/A'), inline=True)
+        embed2 = nextcord.Embed(title=f"{symbol} Income Statement", url=bloomberg_url, description=f"Fiscal Report Date: {the_income_info['fiscalDateEnding']}", color=0x4dff4d)
+        embed2.add_field(name="Interest Expense", value=f"${the_income_info.get('interestExpense', 'N/A')}", inline=False)
+        embed2.add_field(name="Non Interest Income", value=f"${the_income_info.get('nonInterestIncome', 'N/A')}", inline=False)
+        embed2.add_field(name="Other Non Operating Income", value=f"${the_income_info.get('otherNonOperatingIncome', 'N/A')}", inline=False)
+        embed2.add_field(name="Depreciation", value=f"${the_income_info.get('depreciation', 'N/A')}", inline=False)
+        embed2.add_field(name="Depreciation and Amortization", value=f"${the_income_info.get('depreciationAndAmortization', 'N/A')}", inline=False)
+        embed2.add_field(name="Income Before Tax", value=f"${the_income_info.get('incomeBeforeTax', 'N/A')}", inline=False)
+        embed2.add_field(name="Income Tax Expense", value=f"${the_income_info.get('incomeTaxExpense', 'N/A')}", inline=False)
+        embed2.add_field(name="Interest and Debt Expense", value=f"${the_income_info.get('interestAndDebtExpense', 'N/A')}", inline=False)
+        embed2.add_field(name="Net Income from Continuing Operations", value=f"${the_income_info.get('netIncomeFromContinuingOperations', 'N/A')}", inline=False)
+        embed2.add_field(name="Comprehensive Income Net of Tax", value=f"${the_income_info.get('comprehensiveIncomeNetOfTax', 'N/A')}", inline=False)
+        embed2.add_field(name="ebit", value=f"${the_income_info.get('ebit', 'N/A')}", inline=False)
+        embed2.add_field(name="ebitda", value=f"${the_income_info.get('ebitda', 'N/A')}", inline=False)
+        embed2.add_field(name="Net Income", value=f"${the_income_info.get('netIncome', 'N/A')}", inline=False)
 
         if logo_url:
             embed2.set_thumbnail(url=logo_url)
-        embed2.set_footer(text="Data provided by FinancePal Bot")
+        #embed2.set_footer(text="Data provided by FinancePal Bot")
+        current_time = datetime.now().strftime('%m/%d/%Y %H:%M %p')
+        embed2.set_footer(text=f"Data provided by FinancePal Bot | {current_time}")
         embeds.append(embed2)
 
         return embeds
@@ -164,7 +149,7 @@ class CompanyInfoTest(commands.Cog):
         company_name = self.get_company_name_from_ticker(symbol)
         logo_url = self.fetch_logo_image_url(company_name if company_name else symbol)
         if annual_reports:
-            view = DropdownView(annual_reports, interaction, self, logo_url)
+            view = DropdownView(symbol, annual_reports, interaction, self, logo_url)
             await interaction.response.send_message("Select a fiscal date:", view=view)
         else:
             await interaction.response.send_message("No income statement data available for this symbol.", ephemeral=True)
