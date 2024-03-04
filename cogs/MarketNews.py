960e149f0527ecf0838c072a11f703de3946234e
maybekd <karandeepsingh8860@gmail.com>
2024-03-04 18:28:54 -0500

    Updated MarketNews.pyby adding a thumbnail, url, and a footer

--- cogs/MarketNews.py
+++ cogs/MarketNews.py
@@ -4,6 +4,7 @@ from nextcord.ext import commands
 from nextcord import Interaction
 import os
 from dotenv import load_dotenv
+from datetime import datetime
 
 # Load environment variable
 load_dotenv()
@@ -12,6 +13,56 @@ class MarketNews(commands.Cog):
     def __init__(self, client):
         self.client = client
     testServerId = os.getenv('TEST_SERVER_ID')
+    
+    def fetch_logo_image_url(self, symbol):
+        api_key = os.environ['API_NINJAS_KEY'] 
+        api_url_name = f'https://api.api-ninjas.com/v1/logo?name={symbol}'
+        api_url_ticker = f'https://api.api-ninjas.com/v1/logo?name={symbol}&ticker=true'
+        
+        try:
+            response = requests.get(api_url_name, headers={'X-Api-Key': api_key})
+            if response.status_code == 200 and response.json():
+                data = response.json()
+                if 'image' in data[0]:
+                    return data[0]['image']
+
+            response_ticker = requests.get(api_url_ticker, headers={'X-Api-Key': api_key})
+            if response_ticker.status_code == 200 and response_ticker.json():
+                data_ticker = response_ticker.json()
+                if 'image' in data_ticker[0]:
+                    return data_ticker[0]['image']
+
+            print(f"No image found for symbol {symbol}.")
+            return None
+        except requests.RequestException as e:
+            print(f"Request exception: {e}")
+            return None
+
+
+
+    def get_company_name_from_ticker(self, symbol):
+        api_key = os.environ['ALPHA_VANTAGE_API_KEY']
+        endpoint = f'https://www.alphavantage.co/query'
+        params = {
+            'function': 'SYMBOL_SEARCH',
+            'keywords': symbol,
+            'apikey': api_key,
+        }
+
+        try:
+            response = requests.get(endpoint, params=params)
+            if response.status_code == 200 and response.json():
+                data = response.json()
+                if 'bestMatches' in data and data['bestMatches']:
+                    full_name = data['bestMatches'][0]['2. name']
+                    company_name = full_name.split(' ')[0]  # Split by space and take the first part
+                    return company_name
+            return None
+        except requests.RequestException as e:
+            print(f"Error fetching company name: {e}")
+            return None
+
+
     
     def get_news_sentiment(self, symbol):
         try:
@@ -49,9 +100,24 @@ class MarketNews(commands.Cog):
     async def stock(self, interaction: Interaction, symbol: str):
         latest_market_news = self.get_news_sentiment(symbol)
         
+        company_name = self.get_company_name_from_ticker(symbol)
+        logo_url = self.fetch_logo_image_url(company_name if company_name else symbol)
+       
+        
+        bloomberg_url = f"https://www.bloomberg.com/quote/{symbol}:US"
+        
         try:
             if latest_market_news:
-                embed = nextcord.Embed(title=f"Market News for {symbol}", description="Here's the latest market news:", color=0x4dff4d)
+                embed = nextcord.Embed(title=f"Market News for {symbol}", url=bloomberg_url, description="Here's the latest market news:", color=0x4dff4d)
+                if logo_url and logo_url.startswith(("http://", "https://")):
+                    embed.set_thumbnail(url=logo_url)
+                
+                current_time = datetime.now().strftime('%m/%d/%Y %H:%M %p')
+                embed.set_footer(text=f"Data provided by FinancePal Bot | {current_time}")
+                
+                finance_pal_url = "https://styles.redditmedia.com/t5_3nimn/styles/communityIcon_cho9chd8ug431.jpg?format=pjpg&s=e4500f8195a317b675dabd11d245047ac400aa8b"
+                embed.set_author(name="FinancePal", icon_url=finance_pal_url)
+                
                 
                 for item in latest_market_news:
                     # Each item is added as a value of a single field
